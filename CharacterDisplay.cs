837e1963c52ef87b9a59fdaded82b082a16734be
Ashton Barber <33004309+bashtonarber@users.noreply.github.com>
2025-09-02 22:17:45 +0100

    Update CharacterDisplay.cs

--- CharacterDisplay.cs
+++ CharacterDisplay.cs
@@ -7,8 +7,19 @@ using System.IO;
 
 public class CharacterDisplay : MonoBehaviour
 {
+
+/*
+
+This script is for displaying the characters that will be in the Horror Game which will use Scriptable Objects to store the data and pull the info from it. 
+
+This script is to be attached to the Character Display object in Unity with the Scriptable Object required attached. 
+
+*/
+
+    //Declaring the Scriptable Object for us within the script. 
     public ScritableObjectScript character;
-  
+
+    //Declaring the UI elements that will be used from the Scriptable Object.
     public Image characterImage;
     public TextMeshProUGUI characterName;
     public TextMeshProUGUI characterDescription;
@@ -16,11 +27,13 @@ public class CharacterDisplay : MonoBehaviour
 
     void Start()
     {
+        //Finding the required components for each UI element that is attached to the object, including the child objects. 
         characterImage = this.transform.Find("CharacterImage").GetComponent<Image>();
         characterName = this.transform.Find("NameText").GetComponent<TextMeshProUGUI>();
         characterDescription = this.transform.Find("DescriptionText").GetComponent<TextMeshProUGUI>();
         page = this.transform.Find("PageText").GetComponent<TextMeshProUGUI>();
 
+        //Setting the relevant UI elements to what is on the Scriptable Object.
         characterName.text = character.characterName;
         characterDescription.text = character.description;
 
@@ -28,17 +41,6 @@ public class CharacterDisplay : MonoBehaviour
 
         page.text = character.page.ToString();
 
-    }
-
-    void Update()
-    {
-      
-        
-        characterName.text = character.characterName;
-        characterDescription.text = character.description;
-
-        characterImage.sprite = character.picture;
-
-        page.text = character.page.ToString();
     }
 }
+
