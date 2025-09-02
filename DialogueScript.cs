fe9574707f46e2bf4d6f1b5113d13f8739d260cd
Ashton Barber <33004309+bashtonarber@users.noreply.github.com>
2025-09-02 14:25:46 +0100

    Update DialogueScript.cs

--- DialogueScript.cs
+++ DialogueScript.cs
@@ -6,24 +6,38 @@ using System.IO;
 [System.Serializable]
 public class DialogueScript : MonoBehaviour
 {
+
+/*
+
+This is the script that stores the dialogue within the editor, where the dialogue text can be written in the Inspector, along with adding in the characters names.
+
+*/
+
+    //String to set the character names in the Inspector.
     public string name;
     public string name2;
 
+    //Setting the string space for where the sentences can be set out in the Inspector/
     [TextArea(3, 10)]
     public string[] sentences;
 
+    //Setting up another sentence for later in the game.
     [TextArea(3, 10)]
     public string[] sentences2;
 
+    //Setting up another sentence for later in the game.
     [TextArea(3, 10)]
     public string[] sentences3;
 
-    public TextAsset textFileAsset;
+    /*
+    These sections that are commented out was an attempt to grab the text from text files but it ended up throwing an error and when tweaked it wouldn't actually pull the text, so this will be adjusted at a later date. 
+
+        public TextAsset textFileAsset;
 
     public string filePath;
     private int currentLineIndex = 0;
-
-    /*public void Start()
+    
+    public void Start()
     {
         // filePath = Application.dataPath + "/TextFiles/" + "InventoryTextFiles" + "/OldBookDescription.txt";
         //filePath[1] = Application.dataPath + "/TextFiles/" + "InventoryTextFiles" + "/OldBookDescription.txt";
@@ -48,3 +62,4 @@ public class DialogueScript : MonoBehaviour
     }*/
 
 }
+
