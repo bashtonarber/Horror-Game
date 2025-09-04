4b3ec2400c8887a189ac99a0a4c5ec88cbc3b67f
Ashton Barber <33004309+bashtonarber@users.noreply.github.com>
2025-09-04 18:58:08 +0100

    Update InventorySlots.cs

--- InventorySlots.cs
+++ InventorySlots.cs
@@ -3,11 +3,18 @@ using UnityEngine.UI;
 
 public class InventorySlots : MonoBehaviour
 {
+
+/*
+
+This Script is for the slots in the Inventory Menu. It holds the functions for adding and removing items in the UI for the Inventory Menu.
+
+*/
     ItemsScript item;
 
     public Image icon;
     public Button removeButton;
 
+    //This is the function that adds the item and is called when the player collides with an item.
     public void AddItem(ItemsScript newItem)
     {
         item = newItem;
@@ -25,11 +32,13 @@ public class InventorySlots : MonoBehaviour
         removeButton.interactable = false;
     }
 
+   
     public void OnRemoveButton()
     {
         InventoryScript.instance.Remove(item);
     }
 
+    //This function will be used when the use of items has been implemented.
     public void UseItem()
     {
         if (item != null)
