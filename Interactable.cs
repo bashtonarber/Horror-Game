0fde15502444fa1e9f548f01fdc1795fc2c4de7d
Ashton Barber <33004309+bashtonarber@users.noreply.github.com>
2025-09-04 13:56:58 +0100

    Update Interactable.cs

--- Interactable.cs
+++ Interactable.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
    
+
+/*
+
+This script is attached to the item object that gets picked up and then calls the Interact function.
+
+*/
     public virtual void Interact()
     {
-        //Debug.Log("Pickeed");
+        //Debug.Log("Pickeed"); //Old debug to the console to make sure its working.
     }
 
     void Update()
@@ -15,6 +21,7 @@ public class Interactable : MonoBehaviour
     }
 
 
+    //Collision trigger function that calls the Interact function when the player walks into it - This may change to no longer collide but a Button to pick up instead.
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
